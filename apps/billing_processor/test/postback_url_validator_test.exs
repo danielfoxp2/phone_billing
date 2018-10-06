d8c9bbc819c05d32005283625bd5ae4895668834
danielfoxp2 <daniel.foxp2@live.com>
2018-10-06 19:33:17 -0300

    Refactor postback url validator test
    To remove duplicated code

--- apps/billing_processor/test/postback_url_validator_test.exs
+++ apps/billing_processor/test/postback_url_validator_test.exs
@@ -3,15 +3,15 @@ defmodule BillingProcessor.PostbackUrlValidatorTest do
   alias BillingProcessor.PostbackUrlValidator
   doctest PostbackUrlValidator
 
+  @expected_error_message "The processing was not executed because postback url was not informed"
+  
   describe "postback url validator" do
     test "should invalidate when postback url key not exists" do
-      expected_error_message = "The processing was not executed because postback url was not informed"
-      assert PostbackUrlValidator.is_valid?(nil) == {:error, expected_error_message} 
+      assert PostbackUrlValidator.is_valid?(nil) == {:error, @expected_error_message} 
     end
 
     test "should invalidate when postback url is empty" do
-      expected_error_message = "The processing was not executed because postback url was not informed"
-      assert PostbackUrlValidator.is_valid?("") == {:error, expected_error_message} 
+      assert PostbackUrlValidator.is_valid?("") == {:error, @expected_error_message} 
     end
 
     test "should validate when some value is passed" do
