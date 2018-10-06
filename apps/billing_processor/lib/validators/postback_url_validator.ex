313b12aaabcb44ff286c6fe08b303e2db762af15
danielfoxp2 <daniel.foxp2@live.com>
2018-10-06 19:27:28 -0300

    Refactor postback url validator
    To remove duplicated code

--- apps/billing_processor/lib/validators/postback_url_validator.ex
+++ apps/billing_processor/lib/validators/postback_url_validator.ex
@@ -1,7 +1,8 @@
 defmodule BillingProcessor.PostbackUrlValidator do
 
-  def is_valid?(nil), do: {:error, "The processing was not executed because postback url was not informed"}
-  def is_valid?(""), do: {:error, "The processing was not executed because postback url was not informed"}
-  def is_valid?(postback_url), do: {:ok, postback_url}
+  @postback_url_not_exist_message "The processing was not executed because postback url was not informed"
 
+  def is_valid?(nil), do: {:error, @postback_url_not_exist_message}
+  def is_valid?(""), do: {:error, @postback_url_not_exist_message}
+  def is_valid?(postback_url), do: {:ok, postback_url}
 end
