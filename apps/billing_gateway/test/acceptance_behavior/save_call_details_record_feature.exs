c383c7ef465dac4f67ee0c5fa815a1cec6e74f89
danielfoxp2 <daniel.foxp2@live.com>
2018-10-10 19:31:28 -0300

    Specify getting processing protocol number

--- apps/billing_gateway/test/acceptance_behavior/save_call_details_record_feature.exs
+++ apps/billing_gateway/test/acceptance_behavior/save_call_details_record_feature.exs
@@ -3,12 +3,19 @@ defmodule BillingGatewayWeb.SaveCallDetailsRecordFeature do
 
   describe "Save call details records" do
     test "It does not process if there's no postback url", %{conn: conn} do
-
-      params = %{call_records: %{}}
-      conn = post(conn, call_record_path(conn, :create), call_record_params: params)
+      params = %{call_records: []}
+      conn = post(conn, call_record_path(conn, :create), call_records_params: params)
       expected_message = "The processing was not executed because postback url was not informed"
 
       assert json_response(conn, 200)["postback_url_error"] == expected_message
+    end
+
+    test "When there's postback url then a processing protocol is returned to the caller", %{conn: conn} do
+      params = %{call_records: [], postback_url: "www.example.com"}
+      conn = post(conn, call_record_path(conn, :create), call_records_params: params)
+      expected_protocol = 9
+
+      assert json_response(conn, 200)["protocol_number"] == expected_protocol
     end
     
   end
