bcb9e2f944163f1c456d595da07b5beb78e1a591
danielfoxp2 <daniel.foxp2@live.com>
2018-10-16 19:26:11 -0300

    Setup umbrella to run acceptance test across all apps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule PhoneBilling.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -16,5 +17,13 @@ defmodule PhoneBilling.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     []
+  end
+
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test":       ["ecto.drop", "ecto.create", "ecto.migrate", "test"]
+    ]
   end
 end
