c5bfdfb405457e861be2e773b1d71d466221313f
danielfoxp2 <daniel.foxp2@live.com>
2018-10-24 20:10:25 -0300

    Refactor call record validator specification

--- apps/billing_processor/test/call_record_validator_test.exs
+++ apps/billing_processor/test/call_record_validator_test.exs
@@ -4,18 +4,18 @@ defmodule BillingProcessor.CallRecordValidatorTest do
 
   describe "call record validation" do
     test "should invalidate when it does not contains the id" do
-      call_record_without_id = %{"type" => "sfa"}
-      call_record_with_empty_id = %{"id" => "", "type" => "sfa"}
-      call_record_with_nil_id = %{"id" => nil, "type" => "sfa"}
+      call_record_without_id = %{}
+      call_record_with_empty_id = %{"id" => ""}
+      call_record_with_nil_id = %{"id" => nil}
       expected_message_error = "call record don't have id"
 
-      call_record_without_id_gotten = CallRecordValidator.validate(call_record_without_id)
-      call_record_with_empty_id_gotten = CallRecordValidator.validate(call_record_with_empty_id)
-      call_record_with_nil_id_gotten = CallRecordValidator.validate(call_record_with_nil_id)
+      call_record_without_id = CallRecordValidator.validate(call_record_without_id)
+      call_record_with_empty_id = CallRecordValidator.validate(call_record_with_empty_id)
+      call_record_with_nil_id = CallRecordValidator.validate(call_record_with_nil_id)
 
-      assert Enum.member?(call_record_without_id_gotten["errors"], expected_message_error)
-      assert Enum.member?(call_record_with_empty_id_gotten["errors"], expected_message_error)
-      assert Enum.member?(call_record_with_nil_id_gotten["errors"], expected_message_error)
+      assert Enum.member?(call_record_without_id["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_empty_id["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_nil_id["errors"], expected_message_error)
     end
 
     test "should invalidate when it does not contains the type" do
@@ -24,14 +24,14 @@ defmodule BillingProcessor.CallRecordValidatorTest do
       call_record_with_nil_type = %{"type" => nil}
       expected_message_error = "call record don't have type"
 
-      call_record_without_type_gotten = CallRecordValidator.validate(call_record_without_type)
-      call_record_with_empty_type_gotten = CallRecordValidator.validate(call_record_with_empty_type)
-      call_record_with_nil_type_gotten = CallRecordValidator.validate(call_record_with_nil_type)
+      call_record_without_type = CallRecordValidator.validate(call_record_without_type)
+      call_record_with_empty_type = CallRecordValidator.validate(call_record_with_empty_type)
+      call_record_with_nil_type = CallRecordValidator.validate(call_record_with_nil_type)
 
-      assert Enum.member?(call_record_without_type_gotten["errors"], expected_message_error)
-      assert Enum.member?(call_record_with_empty_type_gotten["errors"], expected_message_error)
-      assert Enum.member?(call_record_with_nil_type_gotten["errors"], expected_message_error)
+      assert Enum.member?(call_record_without_type["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_empty_type["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_nil_type["errors"], expected_message_error)
     end
-  end
 
+  end
 end
