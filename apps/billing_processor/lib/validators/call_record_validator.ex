f738c0c3d5d1e109915b3bd11e5a2cb489ac2b5e
danielfoxp2 <daniel.foxp2@live.com>
2018-10-24 19:52:04 -0300

    Validate existence of field type in call record

--- apps/billing_processor/lib/validators/call_record_validator.ex
+++ apps/billing_processor/lib/validators/call_record_validator.ex
@@ -1,6 +1,7 @@
 defmodule BillingProcessor.CallRecordValidator do
   def validate(call_record) do
     validate_id(call_record)
+    |> validate_type()
   end
 
   defp validate_id(call_record) do
@@ -8,20 +9,25 @@ defmodule BillingProcessor.CallRecordValidator do
     |> validate(call_record, :id)
   end
 
+  defp validate_type(call_record) do
+    get_field(call_record, :type)
+    |> validate(call_record, :type)
+  end
+
   defp get_field(call_record, field) do
     Map.get(call_record, field)
   end
 
-  defp validate("", call_record, :id) do
-    errors = Map.get(call_record, :errors, [])
-    Map.put(call_record, "errors", ["call record don't have an id"] ++ errors)
+  defp validate("", call_record, field) do
+    errors = Map.get(call_record, "errors", [])
+    Map.put(call_record, "errors", ["call record don't have #{field}"] ++ errors)
   end
 
-  defp validate(nil, call_record, :id)  do
-    errors = Map.get(call_record, :errors, [])
-    Map.put(call_record, "errors", ["call record don't have an id"] ++ errors)
+  defp validate(nil, call_record, field)  do
+    errors = Map.get(call_record, "errors", [])
+    Map.put(call_record, "errors", ["call record don't have #{field}"] ++ errors)
   end
-  
+
   defp validate(_field_value, call_record, :id), do: call_record
 
 end
--- apps/billing_processor/test/call_record_validator_test.exs
+++ apps/billing_processor/test/call_record_validator_test.exs
@@ -4,13 +4,33 @@ defmodule BillingProcessor.CallRecordValidatorTest do
 
   describe "call record validation" do
     test "should invalidate when it does not contains the id" do
-      call_record_without_id = %{}
-      call_record_with_empty_id = %{"id" => ""}
-      call_record_with_nil_id = %{"id" => nil}
+      call_record_without_id = %{"type" => "sfa"}
+      call_record_with_empty_id = %{"id" => "", "type" => "sfa"}
+      call_record_with_nil_id = %{"id" => nil, "type" => "sfa"}
+      expected_message_error = "call record don't have id"
 
-      assert CallRecordValidator.validate(call_record_without_id) == %{"errors" => ["call record don't have an id"]}
-      assert CallRecordValidator.validate(call_record_with_empty_id) == %{"id" => "", "errors" => ["call record don't have an id"]}
-      assert CallRecordValidator.validate(call_record_with_nil_id) == %{"id" => nil, "errors" => ["call record don't have an id"]}
+      call_record_without_id_gotten = CallRecordValidator.validate(call_record_without_id)
+      call_record_with_empty_id_gotten = CallRecordValidator.validate(call_record_with_empty_id)
+      call_record_with_nil_id_gotten = CallRecordValidator.validate(call_record_with_nil_id)
+
+      assert Enum.member?(call_record_without_id_gotten["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_empty_id_gotten["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_nil_id_gotten["errors"], expected_message_error)
+    end
+
+    test "should invalidate when it does not contains the type" do
+      call_record_without_type = %{}
+      call_record_with_empty_type = %{"type" => ""}
+      call_record_with_nil_type = %{"type" => nil}
+      expected_message_error = "call record don't have type"
+
+      call_record_without_type_gotten = CallRecordValidator.validate(call_record_without_type)
+      call_record_with_empty_type_gotten = CallRecordValidator.validate(call_record_with_empty_type)
+      call_record_with_nil_type_gotten = CallRecordValidator.validate(call_record_with_nil_type)
+
+      assert Enum.member?(call_record_without_type_gotten["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_empty_type_gotten["errors"], expected_message_error)
+      assert Enum.member?(call_record_with_nil_type_gotten["errors"], expected_message_error)
     end
   end
 
