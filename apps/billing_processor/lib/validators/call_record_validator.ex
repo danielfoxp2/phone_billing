18f17d4bcd97ff2241dda5fe373f9739a25c8e8c
danielfoxp2 <daniel.foxp2@live.com>
2018-10-26 19:56:00 -0300

    Fix call record validation behavior
    It was returning false positives outcomes.

--- apps/billing_processor/lib/validators/call_record_validator.ex
+++ apps/billing_processor/lib/validators/call_record_validator.ex
@@ -1,21 +1,15 @@
 defmodule BillingProcessor.CallRecordValidator do
   def validate(call_record) do
     call_record
-    |> validate(:id)
-    |> validate(:type)
-    |> validate(:timestamp)
-    |> validate(:call_id)
-    |> validate(:source)
-    |> validate(:destination)
+    |> validate("id")
+    |> validate("type")
+    |> validate("timestamp")
+    |> validate("call_id")
+    |> validate("source")
+    |> validate("destination")
   end
 
-  defp validate(%{"type" => type} = call_record, :source) when type == "end", do: call_record
-  defp validate(%{"type" => type} = call_record, :source) when type == "start" do
-    call_record
-    |> Map.get(:source)
-    |> validate(call_record, :source)
-  end
-
+  defp validate(%{"type" => type} = call_record, "source") when type == "end", do: call_record
   defp validate(call_record, field) do
     call_record
     |> Map.get(field)
@@ -23,8 +17,8 @@ defmodule BillingProcessor.CallRecordValidator do
   end
 
   defp validate("", in_call_record, field), do: mount_error_for(field, in_call_record)
-  defp validate(nil, in_call_record, field),  do: mount_error_for(field, in_call_record)
-  defp validate(_field_value, call_record, :id), do: call_record
+  defp validate(nil, in_call_record, field), do: mount_error_for(field, in_call_record)
+  defp validate(_field_value, call_record, field), do: call_record
 
   defp mount_error_for(field, in_call_record) do
     errors = Map.get(in_call_record, "errors", [])
