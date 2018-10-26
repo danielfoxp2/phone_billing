40143b082af114196cc06d2720f7979a32d724d0
danielfoxp2 <daniel.foxp2@live.com>
2018-10-26 20:13:45 -0300

    Refactor to ignore unused param

--- apps/billing_processor/lib/validators/call_record_validator.ex
+++ apps/billing_processor/lib/validators/call_record_validator.ex
@@ -19,7 +19,7 @@ defmodule BillingProcessor.CallRecordValidator do
 
   defp validate("", in_call_record, field), do: mount_error_for(field, in_call_record)
   defp validate(nil, in_call_record, field), do: mount_error_for(field, in_call_record)
-  defp validate(_field_value, call_record, field), do: call_record
+  defp validate(_field_value, call_record, _field), do: call_record
 
   defp mount_error_for(field, in_call_record) do
     errors = Map.get(in_call_record, "errors", [])
