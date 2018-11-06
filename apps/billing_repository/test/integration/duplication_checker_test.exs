3f4d223eaf1861caa335268fc29b23e8361c6c21
danielfoxp2 <daniel.foxp2@live.com>
2018-11-06 19:16:10 -0200

    Add behavior to check duplicated call id of call records in database

--- apps/billing_repository/test/integration/duplication_checker_test.exs
+++ apps/billing_repository/test/integration/duplication_checker_test.exs
@@ -10,4 +10,13 @@ defmodule DuplicationCheckerTest do
 
     assert duplicated_keys == expected_result
   end
+
+  test "should return a call_id of a call record for a given call_id if it is duplicated" do
+    call_records = [%{"call_id" => 1}]
+    expected_result = [[call_id: 1]]
+
+    duplicated_keys = DuplicationChecker.for(call_records)
+
+    assert duplicated_keys == expected_result
+  end
 end
