bf041a52d72196511074c05b622f5250fd2781c4
danielfoxp2 <daniel.foxp2@live.com>
2018-11-10 17:03:39 -0200

    Refactor to improve legibility

--- apps/billing_processor/lib/validators/duplication_validator.ex
+++ apps/billing_processor/lib/validators/duplication_validator.ex
@@ -10,10 +10,8 @@ defmodule BillingProcessor.DuplicationValidator do
 
   defp add_errors_for_call_records_ids_duplicated_in(call_records_being_inserted) do
     Enum.map(call_records_being_inserted, fn call_record -> 
-        Enum.count(call_records_being_inserted, fn call_record_to_be_inserted -> 
-          call_record["id"] == call_record_to_be_inserted["id"] 
-        end)
-        |> mount_error_if_needed_for(call_record, "id")
+      count_how_much_duplicated_ids_of_this(call_record, call_records_being_inserted)
+      |> mount_error_if_needed_for(call_record, "id")
     end)
   end
 
@@ -25,13 +23,16 @@ defmodule BillingProcessor.DuplicationValidator do
     end)
   end
 
-  defp has?(database_ids, call_record) do
-    call_record["id"] in database_ids
+  defp count_how_much_duplicated_ids_of_this(call_record, in_call_records_being_inserted) do
+    Enum.count(in_call_records_being_inserted, fn call_record_to_be_inserted -> 
+      call_record["id"] == call_record_to_be_inserted["id"] 
+    end)
   end
 
-  defp mount_error_if_needed_for(false, call_record, _field), do: call_record
-  defp mount_error_if_needed_for(true, in_call_record, field), do: Error.build(in_call_record, {:duplicated_in_database, field}, true)
+  defp has?(database_ids, call_record), do: call_record["id"] in database_ids
 
-  defp mount_error_if_needed_for(1, call_record, _field), do: call_record
+  defp mount_error_if_needed_for(false, in_call_record, _field), do: in_call_record
+  defp mount_error_if_needed_for(1, in_call_record, _field), do: in_call_record
+  defp mount_error_if_needed_for(true, in_call_record, field), do: Error.build(in_call_record, {:duplicated_in_database, field}, true)
   defp mount_error_if_needed_for(_, in_call_record, field), do: Error.build(in_call_record, {:duplicated_in_list_being_inserted, field}, true)
 end
--- apps/billing_processor/lib/validators/error.ex
+++ apps/billing_processor/lib/validators/error.ex
@@ -8,11 +8,11 @@ defmodule BillingProcessor.Error do
 
   defp mount_error(false, in_call_record, _field), do: in_call_record
   defp mount_error(true, in_call_record, {_error_type, field} = field_with_specified_error_type) do
-    ErrorMessage.for_wrong(field_with_specified_error_type, in_call_record[field])
-    |> include(in_call_record)
+    mount_error(in_call_record, field_with_specified_error_type, in_call_record[field])
   end
-  defp mount_error(true, in_call_record, field) do
-    ErrorMessage.for_wrong(field, in_call_record[field])
+  defp mount_error(true, in_call_record, field), do: mount_error(in_call_record, field, in_call_record[field])
+  defp mount_error(in_call_record, field, field_value) do
+    ErrorMessage.for_wrong(field, field_value)
     |> include(in_call_record)
   end
 
