e825b2edd0e8c77efab11a8d9b1feff66e8be70c
danielfoxp2 <daniel.foxp2@live.com>
2018-11-10 20:10:02 -0200

    Refactor duplicity validation of record being inserted and database ones

--- apps/billing_processor/lib/validators/duplication_validator.ex
+++ apps/billing_processor/lib/validators/duplication_validator.ex
@@ -6,9 +6,9 @@ defmodule BillingProcessor.DuplicationValidator do
     call_records_call_ids_from_database = Keyword.get_values(duplicated_persisted_call_records, :call_id)
 
     add_errors_for_call_records_ids_duplicated_in(call_records_being_inserted)
-    |> add_errors_of_duplicated_call_records_in(call_records_ids_from_database)
-    |> add_errors_for_call_ids_duplicated_in
-    |> add_errors_of_duplicated_call_ids_in(call_records_call_ids_from_database)
+    |> add_errors_of_duplicated_call_records_for("id", call_records_ids_from_database)
+    |> add_errors_for_duplicated_call_ids
+    |> add_errors_of_duplicated_call_records_for("call_id", call_records_call_ids_from_database)
   end
 
   defp add_errors_for_call_records_ids_duplicated_in(call_records_being_inserted) do
@@ -18,17 +18,17 @@ defmodule BillingProcessor.DuplicationValidator do
     end)
   end
 
-  defp add_errors_of_duplicated_call_records_in(call_records_being_inserted, duplicated_database_ids) do
+  defp add_errors_of_duplicated_call_records_for(call_records_being_inserted, field, duplicated_database_ids) do
     Enum.map(call_records_being_inserted, fn call_record -> 
       duplicated_database_ids
-      |> has?(call_record)
-      |> mount_error_if_needed_for(call_record, "id")
+      |> has?(field, call_record)
+      |> mount_error_if_needed_for(call_record, field)
     end)
   end
 
-  defp add_errors_for_call_ids_duplicated_in(call_records_being_inserted) do
-    Enum.map(call_records_being_inserted, fn call_record -> 
-      count_how_much_duplicated_call_ids_of_this(call_record, call_records_being_inserted)
+  defp add_errors_for_duplicated_call_ids(in_call_records_being_inserted) do
+    Enum.map(in_call_records_being_inserted, fn call_record -> 
+      count_how_much_duplicated_call_ids_of_this(call_record, in_call_records_being_inserted)
       |> mount_error_if_needed_for(call_record, "call_id")
     end)
   end
@@ -57,11 +57,7 @@ defmodule BillingProcessor.DuplicationValidator do
   defp equals?(nil, _call_record_to_be_inserted), do: false
   defp equals?(call_record_id, call_record_to_be_inserted), do: call_record_id == call_record_to_be_inserted
 
-  defp has?(database_ids, call_record), do: call_record["id"] in database_ids
-
-  defp has_call_id?(database_ids, call_record) do
-    call_record["call_id"] in database_ids
-  end
+  defp has?(database_ids, field, call_record), do: call_record[field] in database_ids
 
   defp mount_error_if_needed_for(false, in_call_record, _field), do: in_call_record
   defp mount_error_if_needed_for(0, in_call_record, _field), do: in_call_record
