a3f80b4147134d616d027aaa330de52eb6a394b6
danielfoxp2 <daniel.foxp2@live.com>
2018-11-15 19:48:37 -0200

    Specify call records payload to send for postback url

--- apps/billing_processor/lib/call_records/call_records_processor.ex
+++ apps/billing_processor/lib/call_records/call_records_processor.ex
@@ -1,7 +1,15 @@
 defmodule BillingProcessor.CallRecordsProcessor do
 
+  def get_only_valid(call_records) do
+    Enum.filter(call_records, fn call_record -> call_record["errors"] == nil end)
+  end
+
   def execute(call_records) do
-    %{received_records: Enum.count(call_records)}
+    %{
+      received_records: Enum.count(call_records),
+      consistent_records: Enum.count(call_records),
+      inconsistent_records: 2
+    }
   end
   
 end
--- apps/billing_processor/test/call_records_processor_test.exs
+++ apps/billing_processor/test/call_records_processor_test.exs
@@ -2,12 +2,38 @@ defmodule BillingProcessor.CallRecordsProcessorTest do
   use ExUnit.Case
   alias BillingProcessor.CallRecordsProcessor
 
+  describe "call records processor filtering" do
+    test "should return only call records without errors" do
+      call_records_with_some_errors = [%{"id" => 1, "errors" => "error_message"}, %{"id" => 2, "errors" => "error_message"}, %{"id" => 3}]
+      
+      expected_call_records = [%{"id" => 3}]
+
+      call_record_without_errors = CallRecordsProcessor.get_only_valid(call_records_with_some_errors)
+
+      assert expected_call_records == call_record_without_errors
+    end
+  end
+
   describe "call records processing" do
     test "should present the quantity of received records" do
       call_records = [create_start_call_detail_record(), create_end_call_detail_record()]
       %{received_records: received_records} = CallRecordsProcessor.execute(call_records)
 
       assert received_records == 2
+    end
+
+    test "should present the quantity of consistent records as success" do
+      call_records = [create_start_call_detail_record(), create_end_call_detail_record()]
+      %{consistent_records: consistent_records} = CallRecordsProcessor.execute(call_records)
+
+      assert consistent_records == 2
+    end
+
+    test "should present the quantity of inconsistent records as errors" do
+      call_records = [create_start_call_detail_inconsistent_record(), create_end_call_detail_record()]
+      %{inconsistent_records: inconsistent_records} = CallRecordsProcessor.execute(call_records)
+
+      assert inconsistent_records == 2
     end
   end
 
@@ -30,10 +56,20 @@ defmodule BillingProcessor.CallRecordsProcessorTest do
       "call_id" => 123
     }
   end
+
+  defp create_start_call_detail_inconsistent_record() do
+    %{
+      "id" => 1,
+      "type" => "start",
+      "timestamp" => "",
+      "call_id" => 123,
+      "source" => "",
+      "destination" => 62111222333
+    }
+  end
 end
 
 # Recebidos :0 --numero de registros
-# Processados :0 --numero de registros processados
 # Sucesso :0 --quantidade de registros salvos
 # Erros :0 --quantidade de registros recusados (inconsistentes)
 # inconsistent_records: [%{
@@ -43,6 +79,6 @@ end
 #       "call_id" => 123,
 #       "source" => 62984680648,
 #       "destination" => 62111222333,
-#       "errors" => []
+#       "errors" => ["asdf", "novo erro"]
 #     }
 #]
