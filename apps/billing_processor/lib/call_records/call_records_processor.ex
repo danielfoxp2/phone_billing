ab0eb72d7ce21963f3418d3067a7e0428685e131
danielfoxp2 <daniel.foxp2@live.com>
2018-11-16 17:41:08 -0200

    Refactor call records processors to improve readability

--- apps/billing_processor/lib/call_records/call_records_processor.ex
+++ apps/billing_processor/lib/call_records/call_records_processor.ex
@@ -8,11 +8,11 @@ defmodule BillingProcessor.CallRecordsProcessor do
   def execute({call_records_inserted, all_call_records}) do
     %{
       received_records_quantity: Enum.count(all_call_records),
-      consistent_records_quantity: Enum.count(call_records_inserted, fn {status, _call_record} -> status == :ok end),
-      inconsistent_records_quantity: Enum.count(all_call_records, fn call_record -> call_record["errors"] != nil end),
-      database_inconsistent_records_quantity: Enum.count(call_records_inserted, fn {status, _call_record} -> status == :error end),
-      failed_records_on_validation: Enum.filter(all_call_records, fn call_record ->  call_record["errors"] != nil end),
-      failed_records_on_insert: Enum.filter(call_records_inserted, fn {status, _call_record} -> status == :error end)
+      consistent_records_quantity: Enum.count(call_records_inserted, &where_everything_is_ok/1),
+      inconsistent_records_quantity: Enum.count(all_call_records, &which_contains_errors/1),
+      database_inconsistent_records_quantity: Enum.count(call_records_inserted, &which_contains_errors/1),
+      failed_records_on_validation: Enum.filter(all_call_records, &which_contains_errors/1),
+      failed_records_on_insert: Enum.filter(call_records_inserted, &which_contains_errors/1)
     }
   end
 
@@ -20,5 +20,10 @@ defmodule BillingProcessor.CallRecordsProcessor do
     Enum.filter(call_records, fn call_record -> call_record["errors"] == nil end)
     |> Enum.group_by(fn call_record -> call_record["call_id"] end)
   end
+
+  defp where_everything_is_ok({status, _not_used}), do: status == :ok
+  
+  defp which_contains_errors({status, _in_call_record}), do: status == :error
+  defp which_contains_errors(in_call_record), do: in_call_record["errors"] != nil
   
 end
