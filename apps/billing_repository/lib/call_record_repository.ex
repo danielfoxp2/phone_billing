f63ede37c9ad480fd3fc777b05d116890d1bb735
danielfoxp2 <daniel.foxp2@live.com>
2018-11-16 13:13:27 -0200

    Make the call insertion be transactional

--- apps/billing_repository/lib/call_record_repository.ex
+++ apps/billing_repository/lib/call_record_repository.ex
@@ -1,16 +1,26 @@
 defmodule BillingRepository.CallRecordRepository do
   alias BillingRepository.Calls.CallRecord
   alias BillingRepository.Repo
+  alias Ecto.Multi
 
-  def insert_only_valid(call_records) do
-    call_records
-    |> Enum.map(fn call_record -> Task.async(fn -> insert_data_of(call_record) end) end)
+  def insert_only_valid(grouped_calls) do
+    grouped_calls
+    |> Enum.map(fn {_not_used, call} -> Task.async(fn -> insert_data_of(call) end) end)
     |> Enum.map(&Task.await/1)
   end
 
-  def insert_data_of(call_record) do
+  def insert_data_of(call) do
+    [first_call_record | [second_call_record]] = call
+
+    Multi.new()
+    |> Multi.run(:start_call, fn _ -> insert(first_call_record) end)
+    |> Multi.run(:end_call, fn _ -> insert(second_call_record) end)
+    |> Repo.transaction()
+  end
+
+  def insert(call_record) do
     %CallRecord{}
-    |> CallRecord.changeset(call_record)
+    |> CallRecord.changeset(call_record) 
     |> Repo.insert()
   end
 
