63802aae28a52ba42b5b963b845c9ddec11fec88
danielfoxp2 <daniel.foxp2@live.com>
2018-11-24 15:46:17 -0200

    Create dummy controller to acceptance testing of postback url

--- apps/billing_gateway/lib/billing_gateway_web/router.ex
+++ apps/billing_gateway/lib/billing_gateway_web/router.ex
@@ -9,5 +9,7 @@ defmodule BillingGatewayWeb.Router do
     pipe_through :api
 
     resources "/call_records", CallRecordController, except: [:new, :edit]
+    get "/dummy_postback_url", DummyPostbackUrlController, :show
+    post "/dummy_postback_url", DummyPostbackUrlController, :create
   end
 end
