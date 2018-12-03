ea66d34895e1fa4ebc6c23132cf2be7bf61f4151
danielfoxp2 <daniel.foxp2@live.com>
2018-12-03 20:23:02 -0200

    Rewrite phone number validator api

--- apps/billing_processor/lib/validators/bill_phone_number_validator.ex
+++ apps/billing_processor/lib/validators/bill_phone_number_validator.ex
@@ -2,15 +2,24 @@ defmodule BillingProcessor.BillPhoneNumberValidator do
   
   @error_message "The bill calculation was not executed because phone number was not informed"
 
-  def is_valid?(nil), do: {:invalid_phone_number, @error_message}
-  def is_valid?(""), do: {:invalid_phone_number, @error_message}
-  def is_valid?(true), do: {:invalid_phone_number, @error_message}
-  def is_valid?(false), do: {:ok}
-  def is_valid?(phone_number) do
-    only_integer_with_ten_or_eleven_digits_regex = ~r/^\d{10,11}+$/
-    validation_phone_number = Regex.match?(only_integer_with_ten_or_eleven_digits_regex, "#{phone_number}") == false
+  def validate(%{"phone_number" => nil} = bill_params), do: mount_error_in(bill_params)
+  def validate(%{"phone_number" => ""} = bill_params), do: mount_error_in(bill_params)
+  def validate(%{"phone_number" => phone_number} = bill_params), do: validate_allowed_range_of(phone_number, bill_params)
+  def validate(bill_params), do: mount_error_in(bill_params)
 
-    validation_phone_number |> is_valid?
+  defp validate_allowed_range_of(phone_number, bill_params) do
+    only_integer_with_ten_or_eleven_digits_regex = ~r/^\d{10,11}+$/
+    variavel = Regex.match?(only_integer_with_ten_or_eleven_digits_regex, "#{phone_number}") == false
+    
+    is_in_the_allowed_range?(variavel, bill_params)
+  end
+
+  defp is_in_the_allowed_range?(false, bill_params), do: bill_params
+  defp is_in_the_allowed_range?(true, bill_params), do: mount_error_in(bill_params)
+
+  defp mount_error_in(bill_params) do
+    errors = Map.get(bill_params, "errors", [])
+    Map.put(bill_params, "errors", [@error_message] ++ errors)
   end
 
 end
--- apps/billing_processor/test/bill_phone_number_validator_test.exs
+++ apps/billing_processor/test/bill_phone_number_validator_test.exs
@@ -4,26 +4,33 @@ defmodule BillingProcessor.BillPhoneNumberValidatorTest do
 
   describe "a valid phone number for bill calculation" do
     test "should invalidate when phone has not AAXXXXXXXX or AAXXXXXXXXX format" do
-      nil_phone_number = nil
-      empty_phone_number = ""
-      invalid_phone_number = "2342x"
+      params_without_phone_number_key = %{}
+      params_with_nil_phone_number = %{"phone_number" => nil}
+      params_with_empty_phone_number = %{"phone_number" => ""}
+      params_with_invalid_phone_number = %{"phone_number" => "2342x"}
 
-      expected_result = {:invalid_phone_number, "The bill calculation was not executed because phone number was not informed"}
+      error_message = "The bill calculation was not executed because phone number was not informed"
+      expected_result_for_inexistent_key = %{"errors" => [error_message]}
+      expected_result_for_nil_number = %{"phone_number" => nil, "errors" => [error_message]}
+      expected_result_for_empty_number = %{"phone_number" => "", "errors" => [error_message]}
+      expected_result_for_invalid_number = %{"phone_number" => "2342x", "errors" => [error_message]}
       
-      assert BillPhoneNumberValidator.is_valid?(nil_phone_number) == expected_result
-      assert BillPhoneNumberValidator.is_valid?(empty_phone_number) == expected_result
-      assert BillPhoneNumberValidator.is_valid?(invalid_phone_number) == expected_result
+      assert BillPhoneNumberValidator.validate(params_without_phone_number_key) == expected_result_for_inexistent_key
+      assert BillPhoneNumberValidator.validate(params_with_nil_phone_number) == expected_result_for_nil_number
+      assert BillPhoneNumberValidator.validate(params_with_empty_phone_number) == expected_result_for_empty_number
+      assert BillPhoneNumberValidator.validate(params_with_invalid_phone_number) == expected_result_for_invalid_number
     end
 
-    test "should validate when phone number has AAXXXXXXXX or AAXXXXXXXXX format" do
-      phone_number_with_ten_digits = "6234568956"
-      phone_number_with_eleven_digits = "62984680648"
+    # test "should validate when phone number has AAXXXXXXXX or AAXXXXXXXXX format" do
+    #   params_with_phone_number_with_ten_digits = %{"phone_number" => "6234568956"}
+    #   params_phone_number_with_eleven_digits = %{"phone_number" => "62984680648"}
 
-      expected_result = {:ok}
+    #   expected_result_number_with_ten_digits = %{"phone_number" => "6234568956"}
+    #   expected_result_number_eleven_digits = %{"phone_number" => "62984680648"}
 
-      assert BillPhoneNumberValidator.is_valid?(phone_number_with_ten_digits) == expected_result
-      assert BillPhoneNumberValidator.is_valid?(phone_number_with_eleven_digits) == expected_result
-    end
+    #   assert BillPhoneNumberValidator.validate(params_with_phone_number_with_ten_digits) == expected_result_number_with_ten_digits
+    #   assert BillPhoneNumberValidator.validate(params_phone_number_with_eleven_digits) == expected_result_number_eleven_digits
+    # end
     
   end
 end
