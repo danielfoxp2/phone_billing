7d4f0e8b5762a0d61f1616960f95fab8c872d7c1
danielfoxp2 <daniel.foxp2@live.com>
2018-12-27 20:09:24 -0200

    Extract algorithm of chargeable minutes to its own module

--- apps/billing_processor/lib/bills/callculator.ex
+++ apps/billing_processor/lib/bills/callculator.ex
@@ -1,27 +1,17 @@
 defmodule BillingProcessor.Bills.Callculator do
+  alias BillingProcessor.Bills.ChargedMinutes
   
   def get_total_of(call, standing_charge, charge_per_minute) do
-    call
-    |> get_duration_in_seconds()
-    |> get_only_accountable_call_minutes()
+    ChargedMinutes.from(call)
     |> calculate_total_minutes_cost_with_this(charge_per_minute)
     |> sum_charged_minutes_with(standing_charge)
-  end
-
-  defp get_duration_in_seconds([start_record, end_record]) do
-    DateTime.diff(end_record.timestamp, start_record.timestamp) 
-  end
-
-  defp get_only_accountable_call_minutes(from_call_duration_in_seconds) do
-    one_minute = 60
-    Integer.floor_div(from_call_duration_in_seconds, one_minute)
   end
 
   defp calculate_total_minutes_cost_with_this(by_minutes, charge_per_minute) do
     charge_per_minute
     |> transform_in_cents()
     |> multiply(by_minutes)
-    |> transform_back_to_reais()
+    |> transform_back_to_brl_currency()
   end
 
   defp sum_charged_minutes_with(total_per_minute, and_standing_charge) do
@@ -30,6 +20,6 @@ defmodule BillingProcessor.Bills.Callculator do
 
   defp transform_in_cents(this_charge_per_minute), do: this_charge_per_minute * 100
   defp multiply(charge_per_minute, by_minute), do: charge_per_minute * by_minute
-  defp transform_back_to_reais(total_call_charge), do: total_call_charge / 100
+  defp transform_back_to_brl_currency(total_call_charge), do: total_call_charge / 100
 
 end
