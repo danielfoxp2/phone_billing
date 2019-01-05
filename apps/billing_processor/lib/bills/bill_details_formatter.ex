457044f3bc88d3dd4b3b86c196fca8e8ede7c4d0
danielfoxp2 <daniel.foxp2@live.com>
2019-01-05 19:15:59 -0200

    Extract formatting bill details to its own module

---
+++ apps/billing_processor/lib/bills/bill_details_formatter.ex
@@ -0,0 +1,18 @@
+defmodule BillingProcessor.Bills.BillDetailsFormatter do
+  
+  def format(start, :start_date), do: "#{format(start.day)}-#{format(start.month)}-#{format(start.year)}"
+  def format(start, :start_time), do: "#{format(start.hour)}:#{format(start.minute)}:#{format(start.second)}"
+  def format(call_duration, :duration) do
+    "#{call_duration.hours}h#{format(call_duration.minutes)}m#{format(call_duration.seconds)}s"
+  end
+  def format(price, :price) do
+    price
+    |> Float.to_string()
+    |> String.replace(".", ",")
+    |> get_currency()
+  end
+
+  defp format(time), do: String.pad_leading("#{time}", 2, "0")
+
+  defp get_currency(of_price), do: "R$ #{of_price}" 
+end
