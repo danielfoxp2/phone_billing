85a03227a981344c72bb9a6ecbcd43d5c57614c0
danielfoxp2 <daniel.foxp2@live.com>
2019-01-05 19:28:14 -0200

    Make parallel get bill details of calls

--- apps/billing_processor/lib/bills/bill_details.ex
+++ apps/billing_processor/lib/bills/bill_details.ex
@@ -6,13 +6,14 @@ defmodule BillingProcessor.Bills.BillDetails do
   def build({calls, taxes}) do
     calls
     |> Map.values()
-    |> Enum.map(fn call -> mount_bill_detail_of(call, taxes) end)
+    |> Enum.map(fn call -> Task.async(fn -> mount_bill_detail_of(call, taxes) end) end)
+    |> Enum.map(&Task.await/1)
   end
 
   defp mount_bill_detail_of([start_call_record, _end_call_record] = call, taxes) do
     %{
       destination: start_call_record.destination,
-      call_start_date: BillDetailsFormatter.format(start_call_record.timestamp, :start_date),
+      call_start_date: BillDetailsFormatter.format(start_call_record.timestamp, :start_date), 
       call_start_time: BillDetailsFormatter.format(start_call_record.timestamp, :start_time),
       call_duration: get_formatted_duration_of(call),
       call_price: get_formatted_price_of(call, taxes)
