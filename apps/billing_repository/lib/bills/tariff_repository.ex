e8378f7a3aba4656cdc95f75f2c0f8329cec5a82
danielfoxp2 <daniel.foxp2@live.com>
2019-01-11 19:47:11 -0200

    Prepare reference informed by the user to search in db

--- apps/billing_repository/lib/bills/tariff_repository.ex
+++ apps/billing_repository/lib/bills/tariff_repository.ex
@@ -1,11 +1,18 @@
 defmodule BillingRepository.Bills.TariffRepository do
   alias BillingRepository.Repo
 
-  def get_taxes(reference_period) do
+  def get_taxes(%{"reference_period" => reference_period}) do
     reference_period
+    |> format_for_db()
     |> get_query()
     |> Repo.query!()
     |> mount_result()
+  end
+
+  defp format_for_db(reference_period) do
+    [month, year] = String.split(reference_period, "/")
+
+    "#{year}#{month}"
   end
 
   defp get_query(reference_period) do
