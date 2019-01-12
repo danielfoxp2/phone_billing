f480c395d12d1f40bb58a3546ab1e8ecf5299c8a
danielfoxp2 <daniel.foxp2@live.com>
2019-01-12 18:48:17 -0200

    Adjust charged minutes module for calculate minutes in different days

--- apps/billing_processor/lib/bills/charged_minutes.ex
+++ apps/billing_processor/lib/bills/charged_minutes.ex
@@ -1,6 +1,38 @@
 defmodule BillingProcessor.Bills.ChargedMinutes do
-  
+  alias BillingProcessor.Bills.BillDetailsFormatter
+
+  # def from(call) do
+  #   call
+  #   |> adjust_start_time_limit()
+  #   |> adjust_end_time_limit()
+  #   |> get_duration_in_seconds()
+  #   |> get_only_accountable_call_minutes()
+  # end
+
   def from(call) do
+    call
+    |> get_how_much_days_in()
+    |> calculate_accountable_minutes_for()
+    # |> adjust_start_time_limit()
+    # |> adjust_end_time_limit()
+    # |> get_duration_in_seconds()
+    # |> get_only_accountable_call_minutes()
+  end
+
+  defp get_how_much_days_in([%{timestamp: start_record_timestamp}, %{timestamp: end_record_timestamp}] = call) do
+    cosmologic_constant = 1
+    initial_day = DateTime.to_date(start_record_timestamp)
+    end_day = DateTime.to_date(end_record_timestamp)
+
+    days_of_call = Date.diff(end_day, initial_day)
+    days_in_call = days_of_call + cosmologic_constant 
+
+    call_with_how_much_days_duration = [%{days: days_in_call} | call]
+    call_with_how_much_days_duration
+  end
+
+  defp calculate_accountable_minutes_for([%{days: 2}, _, _] = call), do: adjust_start_time_limit(call)
+  defp calculate_accountable_minutes_for([%{days: 1}, _, _] = call) do
     call
     |> adjust_start_time_limit()
     |> adjust_end_time_limit()
@@ -8,16 +40,104 @@ defmodule BillingProcessor.Bills.ChargedMinutes do
     |> get_only_accountable_call_minutes()
   end
 
-  defp adjust_start_time_limit([%{timestamp: start_record_timestamp}, end_record]) do
+
+  # Quando a ligação inicia e termina no mesmo dia então basta verificar 
+  # a quantidade de minutos tarifados no próprio dia
+  defp adjust_start_time_limit([%{days: 1} = days, %{timestamp: start_record_timestamp}, end_record]) do
     six_am = "06"
-    new_possible_start_record_timestamp = generate(six_am, start_record_timestamp) 
-    
+    new_possible_start_record_timestamp = generate(six_am, start_record_timestamp)
+
     DateTime.compare(start_record_timestamp, new_possible_start_record_timestamp)
     |> return_start_time_for_this(start_record_timestamp, new_possible_start_record_timestamp)
-    |> mount_start_result(end_record)
+    |> mount_start_result(end_record, days)
   end
 
-  defp adjust_end_time_limit([start_record, %{timestamp: end_record_timestamp}]) do
+  # Como a ligação inicia-se em um dia e termina em outro é necessário verificar 
+  # Quantos minutos foram tarifados para o dia inicial e para o dia final
+  defp adjust_start_time_limit([%{days: 2} = days, %{timestamp: start_record_timestamp}, %{timestamp: end_record_timestamp}]) do
+    IO.puts "Nada a ve"
+    minutos_do_primeiro_dia = montar_call_com_start_record_e_10_pm_como_end_record(start_record_timestamp)
+    |> calcular_minutos
+
+    minutos_do_segundo_dia = montar_call_com_6_am_como_start_e_end_record_timestamp(end_record_timestamp)
+    |> calcular_minutos
+
+    minutos_do_primeiro_dia + minutos_do_segundo_dia
+  end
+
+  #Region montar_call_com_start_record_e_10_pm_como_end_record
+
+  defp montar_call_com_start_record_e_10_pm_como_end_record(start_record_timestamp) do
+    ten_pm = "22"
+    end_record_of_start_day = generate(ten_pm, start_record_timestamp)
+    days = %{days: 1}
+    first_day = [days, %{timestamp: start_record_timestamp}, %{timestamp: end_record_of_start_day}]
+    adjust_start_time_limit(first_day)
+    |> IO.inspect
+  end
+
+  
+
+  #EndRegion montar_call_com_start_record_e_10_pm_como_end_record
+
+
+  #Region montar_call_com_6_am_como_start_e_end_record_timestamp
+
+  defp montar_call_com_6_am_como_start_e_end_record_timestamp(end_record_timestamp) do
+    six_am = "06"
+    start_record_of_end_day = generate(six_am, end_record_timestamp)
+    days = %{days: 1}
+    last_day = [days, %{timestamp: start_record_of_end_day}, %{timestamp: end_record_timestamp}]
+    adjust_start_time_limit(last_day)
+  end
+
+  #EndRegion montar_call_com_6_am_como_start_e_end_record_timestamp
+
+
+  #Region calcular_minutos
+  
+  defp calcular_minutos([_days | call_records_timestamps ]) do
+    call_records_timestamps
+    |> get_duration_in_seconds()
+    |> get_only_accountable_call_minutes()
+  end
+
+  #EndRegion calcular_minutos
+
+
+
+  # Como esse cenário é de quantidade maior que 3 dias então
+  # Calcula os minutos da data inicial e final e soma com as horas dos dias intermediários
+  defp adjust_start_time_limit([days, %{timestamp: start_record_timestamp}, end_record]) do
+    six_am = "06"
+    new_possible_start_record_timestamp = generate(six_am, start_record_timestamp)
+
+    DateTime.compare(start_record_timestamp, new_possible_start_record_timestamp)
+    |> return_start_time_for_this(start_record_timestamp, new_possible_start_record_timestamp)
+    |> mount_start_result(end_record, days)
+  end
+
+  defp adjust_end_time_limit([%{days: 1}, start_record, %{timestamp: end_record_timestamp}]) do
+    ten_pm = "22"
+    new_possible_end_record_timestamp = generate(ten_pm, end_record_timestamp) 
+
+    DateTime.compare(end_record_timestamp, new_possible_end_record_timestamp)
+    |> return_end_time_for_this(end_record_timestamp, new_possible_end_record_timestamp)
+    |> mount_end_result(start_record)
+  end
+
+  defp adjust_end_time_limit([%{days: 2}, start_record, %{timestamp: end_record_timestamp}]) do
+    ten_pm = "22"
+    new_possible_end_record_timestamp = generate(ten_pm, end_record_timestamp) 
+
+    DateTime.compare(end_record_timestamp, new_possible_end_record_timestamp)
+    |> return_end_time_for_this(end_record_timestamp, new_possible_end_record_timestamp)
+    |> mount_end_result(start_record)
+  end
+
+  # quantidade_dias * (14 * 60)
+
+  defp adjust_end_time_limit([_dont_matter, start_record, %{timestamp: end_record_timestamp}]) do
     ten_pm = "22"
     new_possible_end_record_timestamp = generate(ten_pm, end_record_timestamp) 
 
@@ -27,9 +147,9 @@ defmodule BillingProcessor.Bills.ChargedMinutes do
   end
 
   defp generate(hour, date) do
-    date_as_iso = "#{date.year}-#{date.month}-#{date.day}T#{hour}:00:00Z"
+    date_as_iso = "#{BillDetailsFormatter.format(date, :date_y_m_d)}T#{hour}:00:00Z"
     {:ok, converted_date, 0} = DateTime.from_iso8601(date_as_iso)
-
+    
     converted_date
   end
 
@@ -37,19 +157,20 @@ defmodule BillingProcessor.Bills.ChargedMinutes do
     DateTime.diff(end_record.timestamp, start_record.timestamp) 
   end
 
+  defp get_only_accountable_call_minutes(from_call_duration_in_seconds) when from_call_duration_in_seconds < 60, do: 0
   defp get_only_accountable_call_minutes(from_call_duration_in_seconds) do
     one_minute = 60
     Integer.floor_div(from_call_duration_in_seconds, one_minute)
   end
 
-  defp return_start_time_for_this(:lt, start_record_timestamp, six_am), do: six_am
-  defp return_start_time_for_this(equal_or_greater_result, start_record_timestamp, six_am), do: start_record_timestamp
+  defp return_start_time_for_this(:lt, _start_record_timestamp, six_am), do: six_am
+  defp return_start_time_for_this(_equal_or_greater_result, start_record_timestamp, _six_am), do: start_record_timestamp
   
-  defp return_end_time_for_this(:lt, end_record_timestamp, ten_pm), do: end_record_timestamp
-  defp return_end_time_for_this(equal_or_greater_result, end_record_timestamp, ten_pm), do: ten_pm
+  defp return_end_time_for_this(:lt, end_record_timestamp, _ten_pm), do: end_record_timestamp
+  defp return_end_time_for_this(_equal_or_greater_result, _end_record_timestamp, ten_pm), do: ten_pm
 
-  defp mount_start_result(start_record_timestamp, end_record) do
-    [%{timestamp: start_record_timestamp}, end_record]
+  defp mount_start_result(start_record_timestamp, end_record, days_in_call) do
+    [days_in_call, %{timestamp: start_record_timestamp}, end_record]
   end
 
   defp mount_end_result(end_record_timestamp, start_record) do
--- apps/billing_processor/test/charged_minutes_test.exs
+++ apps/billing_processor/test/charged_minutes_test.exs
@@ -38,6 +38,24 @@ defmodule BillingProcessor.ChargedMinutesTest do
     end
   end
 
+  describe "charged minutes from call when call starts after 21:59:59 in one day but ends before 06:00:00 of the next day" do
+    test "should return zero minutes because it is in reduced time" do
+      call = get_a_call_starting_after_10_pm_but_ending_before_6_am()
+      expected_result = 0
+
+      assert ChargedMinutes.from(call) == expected_result
+    end
+  end
+
+  describe "charged minutes from call when call starts before 22:00:00 in one day and ends after 06:00:00 of the next day" do
+    test "should return only accountable minutes" do
+      call = get_a_call_starting_after_9_50_pm_and_ending_8_10_am_of_the_next_day()
+      expected_result = 140
+
+      assert ChargedMinutes.from(call) == expected_result
+    end
+  end
+
   defp get_a_five_minutes_call_in_stardard_time_call() do
     [
       %{type: "start", call_id: 1, timestamp: get_date_time("2018-10-31T06:00:00Z")},
@@ -63,6 +81,20 @@ defmodule BillingProcessor.ChargedMinutesTest do
     [
       %{type: "start", call_id: 1, timestamp: get_date_time("2018-10-31T05:55:00Z")},
       %{type: "end", call_id: 1, timestamp: get_date_time("2018-10-31T06:05:00Z")}
+    ]
+  end
+
+  defp get_a_call_starting_after_10_pm_but_ending_before_6_am() do
+    [
+      %{type: "start", call_id: 1, timestamp: get_date_time("2018-10-31T23:50:00Z")},
+      %{type: "end", call_id: 1, timestamp: get_date_time("2018-11-01T00:08:05Z")}
+    ]
+  end
+
+  defp get_a_call_starting_after_9_50_pm_and_ending_8_10_am_of_the_next_day do
+    [
+      %{type: "start", call_id: 1, timestamp: get_date_time("2018-10-31T21:50:00Z")},
+      %{type: "end", call_id: 1, timestamp: get_date_time("2018-11-01T08:10:05Z")}
     ]
   end
 
