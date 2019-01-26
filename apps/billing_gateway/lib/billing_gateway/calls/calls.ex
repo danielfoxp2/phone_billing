4c354fcfc6ff7ec0a07257d14194e7c949ca4c73
danielfoxp2 <daniel.foxp2@live.com>
2019-01-26 18:50:11 -0200

    Remove unnecessary code

--- apps/billing_gateway/lib/billing_gateway/calls/calls.ex
+++ apps/billing_gateway/lib/billing_gateway/calls/calls.ex
@@ -15,47 +15,6 @@ defmodule BillingGateway.Calls do
   alias BillingProcessor.ResponseBuilder
   alias BillingGateway.SendResponse
 
-  @doc """
-  Returns the list of call_records.
-
-  ## Examples
-
-      iex> list_call_records()
-      [%CallRecord{}, ...]
-
-  """
-  def list_call_records do
-    Repo.all(CallRecord)
-  end
-
-  @doc """
-  Gets a single call_record.
-
-  Raises `Ecto.NoResultsError` if the Call record does not exist.
-
-  ## Examples
-
-      iex> get_call_record!(123)
-      %CallRecord{}
-
-      iex> get_call_record!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_call_record!(id), do: Repo.get!(CallRecord, id)
-
-  @doc """
-  Creates a call_record.
-
-  ## Examples
-
-      iex> create_call_record(%{field: value})
-      {:ok, %CallRecord{}}
-
-      iex> create_call_record(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_call_record(call_records_params) do
     get_postback_url_from(call_records_params)
     |> PostbackUrlValidator.is_valid?
@@ -85,50 +44,5 @@ defmodule BillingGateway.Calls do
   end
   
   defp get_protocol_number(), do: Protocol.new_number
-  @doc """
-  Updates a call_record.
-
-  ## Examples
-
-      iex> update_call_record(call_record, %{field: new_value})
-      {:ok, %CallRecord{}}
-
-      iex> update_call_record(call_record, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_call_record(%CallRecord{} = call_record, attrs) do
-    call_record
-    |> CallRecord.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a CallRecord.
-
-  ## Examples
-
-      iex> delete_call_record(call_record)
-      {:ok, %CallRecord{}}
-
-      iex> delete_call_record(call_record)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_call_record(%CallRecord{} = call_record) do
-    Repo.delete(call_record)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking call_record changes.
-
-  ## Examples
-
-      iex> change_call_record(call_record)
-      %Ecto.Changeset{source: %CallRecord{}}
-
-  """
-  def change_call_record(%CallRecord{} = call_record) do
-    CallRecord.changeset(call_record, %{})
-  end
+  
 end
--- apps/billing_gateway/lib/billing_gateway_web/controllers/call_record_controller.ex
+++ apps/billing_gateway/lib/billing_gateway_web/controllers/call_record_controller.ex
@@ -6,44 +6,10 @@ defmodule BillingGatewayWeb.CallRecordController do
 
   action_fallback BillingGatewayWeb.FallbackController
 
-  def index(conn, _params) do
-    #call_records = Calls.list_call_records()
-    call_records = [%{
-          id: 1,
-          call_id: 1,
-          destination: "+5562984680648",
-          source: "+5562984680648",
-          timestamp: "20181004235619",
-          type: 1
-    }]
-
-    render(conn, "index.json", call_records: call_records)
-  end
-
   def create(conn, %{"call_records_params" => call_records_params}) do
     with {:ok, protocol_number} <- Calls.create_call_record(call_records_params) do
       conn
       |> render("show.json", protocol_number: protocol_number)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    call_record = Calls.get_call_record!(id)
-    render(conn, "show.json", call_record: call_record)
-  end
-
-  def update(conn, %{"id" => id, "call_record" => call_record_params}) do
-    call_record = Calls.get_call_record!(id)
-
-    with {:ok, %CallRecord{} = call_record} <- Calls.update_call_record(call_record, call_record_params) do
-      render(conn, "show.json", call_record: call_record)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    call_record = Calls.get_call_record!(id)
-    with {:ok, %CallRecord{}} <- Calls.delete_call_record(call_record) do
-      send_resp(conn, :no_content, "")
     end
   end
 end
