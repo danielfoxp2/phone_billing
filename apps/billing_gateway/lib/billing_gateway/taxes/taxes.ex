95ca3c1fde288e7455e11d2155629e8a8c2cc390
danielfoxp2 <daniel.foxp2@live.com>
2019-01-26 12:21:10 -0200

    Finish orchestration of taxes insertion

--- apps/billing_gateway/lib/billing_gateway/taxes/taxes.ex
+++ apps/billing_gateway/lib/billing_gateway/taxes/taxes.ex
@@ -1,11 +1,18 @@
 defmodule BillingGateway.Taxes do
   alias BillingRepository.Bills.TariffRepository
+  alias BillingProcessor.TaxesReferenceValidator
   alias BillingProcessor.TaxesValidator
 
   def create(taxes_params) do
     taxes_params
     |> TaxesValidator.validate()
+    |> validate_if_reference_can_be_updated()
     |> insert_new_taxes_when_there_is_no_error()
+  end
+
+  defp validate_if_reference_can_be_updated(taxes_params) do
+    TariffRepository.get_taxes(taxes_params)
+    |> TaxesReferenceValidator.validate(taxes_params)
   end
 
   defp insert_new_taxes_when_there_is_no_error(%{"errors" => errors_reasons}), do: {:taxes_error, errors_reasons}
