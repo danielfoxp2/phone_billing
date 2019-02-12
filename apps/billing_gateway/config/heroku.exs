a8a74aee567511f7bbbfff48b9fedab33b84da46
danielfoxp2 <daniel.foxp2@live.com>
2019-02-12 19:28:05 -0200

    Remove cache_manifest
    As this project does not have a frontend it is not needed.

--- apps/billing_gateway/config/heroku.exs
+++ apps/billing_gateway/config/heroku.exs
@@ -5,7 +5,6 @@ config :billing_gateway, BillingGatewayWeb.Endpoint,
   url: [scheme: "https", host: "peaceful-coast-39242.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   http: [port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :logger, level: :info
