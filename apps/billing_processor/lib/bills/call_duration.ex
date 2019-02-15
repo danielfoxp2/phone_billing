072b9031cb289a40cf84d012888b9063664977f1
danielfoxp2 <daniel.foxp2@live.com>
2019-02-15 19:41:13 -0200

    Refactor call duration to remove magic numbers

--- apps/billing_processor/lib/bills/call_duration.ex
+++ apps/billing_processor/lib/bills/call_duration.ex
@@ -1,5 +1,8 @@
 defmodule BillingProcessor.Bills.CallDuration do
   
+  @seconds_in_days 86400
+  @hours_in_day 24
+
   def of(call) do
     call
     |> get_duration_in_seconds()
@@ -11,10 +14,10 @@ defmodule BillingProcessor.Bills.CallDuration do
     DateTime.diff(end_record.timestamp, start_record.timestamp) 
   end
 
-  defp get_time(seconds) when seconds < 86400, do: Time.add(~T[00:00:00], seconds)
+  defp get_time(seconds) when seconds < @seconds_in_days, do: Time.add(~T[00:00:00], seconds)
   defp get_time(seconds) do
     minutes_and_seconds = Time.add(~T[00:00:00], seconds)
-    hours =  Integer.floor_div(seconds, 86400) * 24
+    hours =  Integer.floor_div(seconds, @seconds_in_days) * @hours_in_day
 
     %{hour: hours, minute: minutes_and_seconds.minute, second: minutes_and_seconds.second}
   end
