52709faa264993521c6c892ab949c7b53a8b6b40
danielfoxp2 <daniel.foxp2@live.com>
2019-02-16 17:18:13 -0200

    Create documentation for protocol

--- apps/billing_repository/lib/protocol.ex
+++ apps/billing_repository/lib/protocol.ex
@@ -1,9 +1,21 @@
 defmodule BillingRepository.Protocol do
   alias BillingRepository.Repo
+  
+  @moduledoc """
+    Creates a protocol number to identify the chunk of call records processed. 
+  """
 
-  def new_number do
+  @doc """
+  Generate a new protocol number
+
+  ## Examples
+
+      iex> Protocol.new_number()
+      3
+  """
+  def new_number() do
     Ecto.Adapters.SQL.query!(Repo, "select nextval('protocol_seq')")
-    |> get_number
+    |> get_number()
   end
 
   defp get_number(%Postgrex.Result{rows: rows}) do
