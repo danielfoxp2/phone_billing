db366fe944a92743b3b11291ed13becb5fe7aaba
danielfoxp2 <daniel.foxp2@live.com>
2019-02-16 18:11:38 -0200

    Create documentation for calls

--- apps/billing_gateway/lib/billing_gateway/calls/calls.ex
+++ apps/billing_gateway/lib/billing_gateway/calls/calls.ex
@@ -1,8 +1,4 @@
 defmodule BillingGateway.Calls do
-  @moduledoc """
-  The Calls context.
-  """
-
   alias BillingRepository.Protocol
   alias BillingRepository.DatabaseDuplication
   alias BillingRepository.CallRecordRepository
@@ -12,6 +8,47 @@ defmodule BillingGateway.Calls do
   alias BillingProcessor.CallStructure
   alias BillingProcessor.ResponseBuilder
   alias BillingGateway.SendResponse
+
+  @moduledoc """
+  Orchestrates persistence flow of call records 
+  """
+
+  @doc """
+  Given a call record params, it calls the responsibles for:
+  
+  - validates the postback url;
+  - process insertion of the given call records 
+
+  ## Parameters
+
+    A map with the following structure:
+
+    - postback url: a url to send the call records processing result ;
+    - call records: a list of call records.
+
+  ## Examples
+
+      iex> call_records =  [
+      > %{
+      >   "id" => "1",
+      >   "type" => "start",
+      >   "timestamp" => "2018-11-15T13:15:44Z",
+      >   "call_id" => "1",
+      >   "source" => "62984680648",
+      >   "destination" => "62111222333"
+      >   },
+      >   %{
+      >     "id" => "2",
+      >     "type" => "end",
+      >     "timestamp" => "2018-11-15T13:23:14Z",
+      >     "call_id" => "1"
+      >   }
+      > ]
+
+      iex> call_record_params = %{"postback_url" => "www.example.com", "call_records" => call_records}
+      iex> Calls.create_call_record(bill_params)
+      {:ok, 3}
+  """
 
   def create_call_record(call_records_params) do
     get_postback_url_from(call_records_params)
