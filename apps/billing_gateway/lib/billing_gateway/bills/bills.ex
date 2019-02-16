14e02f067be5d9c9b32c91a4404fd35371e04dd2
danielfoxp2 <daniel.foxp2@live.com>
2019-02-16 17:41:26 -0200

    Create documentation for bills

--- apps/billing_gateway/lib/billing_gateway/bills/bills.ex
+++ apps/billing_gateway/lib/billing_gateway/bills/bills.ex
@@ -6,6 +6,47 @@ defmodule BillingGateway.Bills do
   alias BillingProcessor.Bills.FullBill
   alias BillingProcessor.Bills.BillReference
 
+  @moduledoc """
+  Orchestrates the flow of bill calculation
+  """
+
+  @doc """
+  Given a bill params, it calls the responsibles for:
+  
+  - get the reference to calculate the bill;
+  - validates the given phone number;
+  - validates the given reference;
+  - calculates the bill;
+
+  ## Parameters
+
+    A map with the following structure:
+
+    - phone number: a String with ten or eleven digits;
+    - reference period (optional): a String in the format `"MM/yyyy"`.
+
+  ## Examples
+
+      iex> bill_params = %{"phone_number" => "62984680648", "reference_period" => "11/2018"}
+      iex> Bills.calculate(bill_params)
+      > %{
+      >   "phone_number" => "62984680648", 
+      >   "reference_period" => "11/2018",
+      >   "bill" => %{
+      >     "bill_total" => "R$ 0,54",
+      >     "bill_details" => [
+      >       %{
+      >         "destination" => 6298457834,
+      >         "call_start_date" => "23-11-2018",
+      >         "call_start_time" => "21:57:13",
+      >         "call_duration" => "0h20m40s",
+      >         "call_price" => "R$ 0,54"
+      >       }
+      >     ]
+      >   }
+      > }
+  """
+
   def calculate(bill_params) do
     bill_params
     |> BillReference.get_last_month_if_needed(Date.utc_today())
