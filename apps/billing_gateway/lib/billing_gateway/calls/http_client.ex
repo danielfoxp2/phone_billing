318e23ee3390dc63adc859fe9aeab059dfa4d092
danielfoxp2 <daniel.foxp2@live.com>
2019-02-16 18:21:24 -0200

    Create documentation for modules that send response to postback url

--- apps/billing_gateway/lib/billing_gateway/calls/http_client.ex
+++ apps/billing_gateway/lib/billing_gateway/calls/http_client.ex
@@ -1,5 +1,12 @@
 defmodule BillingGateway.HttpClient do
-  
+  @moduledoc """
+    Sends result of call records processed to the postback url  
+  """
+
+  @doc """
+  Sends result of call records processed to the postback url
+
+  """
   def post(postback_url, call_records_process_result_as_json, headers) do
     HTTPoison.post(postback_url, call_records_process_result_as_json, headers)
   end
--- apps/billing_gateway/lib/billing_gateway/calls/send_response.ex
+++ apps/billing_gateway/lib/billing_gateway/calls/send_response.ex
@@ -1,7 +1,15 @@
 defmodule BillingGateway.SendResponse do
 
   @http_client Application.get_env(:billing_gateway, :http_client)
+  
+  @moduledoc """
+    Mount json data with processed call records to be sent to postback url
+  """
 
+  @doc """
+  Mount json data with processed call records to be sent to postback url
+
+  """
   def to(call_records_process_result, postback_url) do
     headers = ["content-type": "application/json"]
     @http_client.post(postback_url, get_json_from(call_records_process_result), headers)
