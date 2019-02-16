4b4c1d19b4c900d7d6db331ed06b63c7a7a0d7a3
danielfoxp2 <daniel.foxp2@live.com>
2019-02-16 20:44:44 -0200

    Create documentation of bill_reference

--- apps/billing_processor/lib/bills/bill_reference.ex
+++ apps/billing_processor/lib/bills/bill_reference.ex
@@ -1,5 +1,34 @@
 defmodule BillingProcessor.Bills.BillReference do
   
+  @moduledoc """
+  Responsible of getting the informed reference or the previous one if no reference is informed
+  """
+
+  @doc """
+  When the user does not inform a reference to calculate the bill, this function returns the 
+  previous reference of the current month, otherwise, it returns the informed reference.
+
+  ## Parameters
+
+    - params: a map containing the reference_period key informed by the client
+    - current_reference: the current reference to be used to return the previous related reference if the client does not inform any reference_period
+  
+  ## Examples
+
+  ### With reference period informed
+
+      iex> params = %{"reference_period" => "11/2018"}
+      iex> {:ok, current_reference} = Date.new(2019, 01, 01)
+      iex> BillReference.get_last_month_if_needed(params, current_reference)
+      %{"reference_period" => "11/2018"}
+
+  ### Without reference period informed
+    
+      iex> params = %{}
+      iex> {:ok, current_reference} = Date.new(2019, 01, 01)
+      iex> BillReference.get_last_month_if_needed(params, current_reference)
+      %{"reference_period" => "12/2018"}
+  """
   def get_last_month_if_needed(%{"reference_period" => nil} = params, current_reference) do
     put_last_reference_in(params, current_reference)
   end
